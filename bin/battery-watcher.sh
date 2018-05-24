c546a1ad461c04bca96d85f195cfd631acdf62c5
Leonardo Strozzi <laps15@inf.ufpr.br>
2018-05-24 00:37:07 -0300

    Implements full battery notification
    Signed-off-by: Leonardo Strozzi <laps15@inf.ufpr.br>

--- bin/battery-watcher.sh
+++ bin/battery-watcher.sh
@@ -3,6 +3,7 @@
 BAT_LOW_ICON='/usr/share/icons/Adwaita/scalable/status/battery-low-symbolic.svg'
 BATTERIES='/sys/class/power_supply/BAT*/uevent'
 TMP_FILE='/tmp/batfile.sh'
+FULL_NOTIFIED=0
 
 __beep() {
     (speaker-test -t sine -f 1000 &) >/dev/null;
@@ -17,7 +18,10 @@ genSrcFile() {
 getLevel() {
     source ${TMP_FILE};
     if [ "${POWER_SUPPLY_STATUS}" == "Charging" ]; then
+        FULL_NOTIFIED=0
         echo '-1';
+    elif [ "${POWER_SUPPLY_STATUS}" == "Full" ]; then
+        echo -2;
     else
         echo "${POWER_SUPPLY_CAPACITY}";
     fi
@@ -31,14 +35,23 @@ main() {
     while [ 0 ]; do
         genSrcFile
         bat_lvl=$( getLevel );
-        if [ ! ${bat_lvl} -eq -1 ]; then
+        if [ ${bat_lvl} -gt 0 ]; then
             if [ ${bat_lvl} -lt ${thresh_hold} ]; then
                 [ ${must_beep} -eq 1 ] &&\
                 (__beep; __beep; __beep)&
-                opts="-u critical -t 7770"
+                opts="-u critical -t 2000"
                 summary="Battery level is bellow ${thresh_hold}%"
                 text="Please charge"
                 notify-send ${opts} "${summary}" "${text}"
+            fi
+        else
+            if [ $FULL_NOTIFIED -eq 0 ]; then
+                FULL_NOTIFIED=1
+                if [ ${bat_lvl} -eq -2 ]; then
+                    opts="-u critical -t 5000"
+                    summary="Battery is already full"
+                    notify-send ${opts} "${summary}" "${text}"
+                fi
             fi
         fi
         sleep ${interval}
@@ -46,20 +59,20 @@ main() {
 }
 
 tryLock() {
-    flock -xn 777 
+    flock -xn 200 
     return $?
 }
 
 tryUnlock() {
-    flock -u 777
+    flock --unlock 200
 }
 
 (
     summary="Battery watcher already running!"
-    opts="-u critical -t 7770"
+    opts="-u critical -t 2000"
     tryLock || { notify-send ${opts} "${summary}"; exit 2; }
-    trap tryUnlock EXIT
+    trap tryUnlock INT EXIT
 
-    main 777>&-
+    main 200>&-
 
-) 777>"${HOME}/.i3/.batwatch.lock"
+) 200>"${HOME}/.i3/.batwatch.lock"
